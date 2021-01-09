2b69410b28b5adc2ee50ae458788de830d4f4325
freilinjb <freilinjb@gmail.com>
2021-01-09 01:39:53 -0400

    insert, update and delete users probado en POSTMAN

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,23 +1,99 @@
-const { create } = require('./user.service');
-const { genSaltSync, hashSync } = require('bcrypt');
+const {
+  create,
+  getUserByUserId,
+  getUsers,
+  updateUser,
+  deleteUser,
+} = require("./user.service");
+const { genSaltSync, hashSync } = require("bcrypt");
 
 module.exports = {
-    createUser: (req, res) => {
-        const body = req.body;
-        const salt = genSaltSync(10);
-        body.password = hashSync(body.password, salt);
-        create(body, (err, results) => {
-            if(err) {
-                console.log(err);
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
-            }
-            return res.status(200).json({
-                success: 1,
-                data: results
-            });
+  createUser: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    create(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
         });
-    }
-}
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getUserByUserId: (req, res) => {
+    const id = req.params.id;
+    getUserByUserId(id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Record not Found",
+        });
+      }
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getUsers: (req, res) => {
+    getUsers((err, results) => {
+      if (err) {
+        return console.log(err);
+      }
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  updateUser: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    updateUser(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    //   if(!results) {
+    //       return res.json({
+    //           success: 0,
+    //           message:"Failet to update user"
+    //       });
+    //   }
+      return res.json({
+        success: 1,
+        message: "update succesfully",
+      });
+    });
+  },
+  deleteUser: (req, res) => {
+    const data = req.body;
+    deleteUser(data, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Record Not Found",
+        });
+      }
+      return res.json({
+        success: 1,
+        message: "user deleted succesfully",
+      });
+    });
+  },
+};
--- api/users/user.service.js
+++ api/users/user.service.js
@@ -1,24 +1,71 @@
-const pool = require('../../config/database');
+const pool = require("../../config/database");
 
 module.exports = {
-    create: (data, callback) => {
-        pool.query(
-            `INSERT INTO registration (firstName,lastName, gender, email, password, number) VALUES(?,?,?,?,?,?)`,
-            [
-                data.first_name,
-                data.last_name,
-                data.gender,
-                data.email,
-                data.password,
-                data.number,
-            ],
-            (error, result, fields) => {
-                if(error) {
-                    return callback(error);
-                }
+  create: (data, callback) => {
+    pool.query(
+      `INSERT INTO registration (firstName,lastName, gender, email, password, number) VALUES(?,?,?,?,?,?)`,
+      [
+        data.first_name,
+        data.last_name,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+      ],
+      (error, result, fields) => {
+        if (error) {
+          return callback(error);
+        }
 
-                return callback(null, result);
-            }
-        );
-    }
-}
+        return callback(null, result);
+      }
+    );
+  },
+
+  getUsers: (callback) => {
+    pool.query(`SELECT id,lastName,firstName,gender,email,number from registration`, [], (error, result, fields) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, result);
+    });
+  },
+  getUserByUserId: (id, callback) => {
+    pool.query(`SELECT * FROM registration WHERE id = ?`, [id], (error, result, fields) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, result[0]);
+    });
+  },
+  updateUser: (data, callback) => {
+    pool.query(
+      `update registration SET firstName=?, lastName=?, gender=?,email=?, password=?,number=? WHERE id = ?`,
+      [
+        data.first_name,
+        data.last_name,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+        data.id,
+      ],
+      (error, result, fields) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, result[0]);
+      }
+    );
+  },
+  deleteUser: (data, callback) => {
+    `DELETE FROM registration WHERE id = ?`,
+      [data.id],
+      (error, result, fields) => {
+        if (error) {
+          callback(error);
+        }
+        return callback(null, result[0]);
+      };
+  },
+};
