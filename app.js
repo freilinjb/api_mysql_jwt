a65622003d523c49268336caf32f1cabfc94c033
freilinjb <freilinjb@gmail.com>
2021-01-08 23:38:32 -0400

    Iniciando el proyecto

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//callback
+app.get("/api", (req, res) => {
+    res.json({
+        success: 1,
+        message: 'This is rest apis working'
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
